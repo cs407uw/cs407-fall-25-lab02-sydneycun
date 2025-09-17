81576ff519c00d8bde950ee9521a7441c9bbc703
sydneycunningham <scunningham6@wisc.edu>
2025-09-17 16:23:12 -0500

    initial changes

--- cardfolio/app/src/main/java/com/cs407/cardfolio/MainActivity.kt
+++ cardfolio/app/src/main/java/com/cs407/cardfolio/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cs407.cardfolio.ui.theme.CardfolioTheme
@@ -19,29 +21,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CardfolioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ){
+
                 }
+
+
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Cardfolio() {
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CardfolioPreview() {
     CardfolioTheme {
-        Greeting("Android")
+        Cardfolio()
     }
 }
